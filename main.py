97657b52349ef390b8cf72a2296c8ddec456b1cc
Anupam Prakash <anupamprakashvk@gmail.com>
2026-08-12 12:34:03 +0530

    main

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import func
+
+from database import engine,Base, get_db
+from models import Expense
+
+Base.metadata.create_all(bind=engine)
+
+app=FastAPI(
+    title="Expense Management Application",
+    description="FastAPI Expense Management Application",
+    version="1.0.0"
+    )
+    
+@app.get("/")
+async def root():
+    return{"message":"Expense Management API"}
+    
+@app.post("/expenses/")
+async def create_expense(
+    expense:ExpenseCreate,
+    db:Session =Depends(get_db):
+        new_expense=Expense(
+            name=expexse.name
+            amount=expense.amount
+            category=expense.category
+            )
+        db.add(new_expense)
+        db.commit()
+        db.refresh(new_expense)
+        
+        return new-expense
+    )
+    
+@app.get("/expenses/month/{year}/{month}")
+async def get_expenses_by_month(
+    year:int,
+    month:int,
+    db:    )
+
+
+  
+    
